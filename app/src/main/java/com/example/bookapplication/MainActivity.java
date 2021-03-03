3a44137869d296ef6e6432d0d219fc5aacce46ed
katesune <katesune.akk>
2021-03-03 15:41:18 +0800

    Example BookApplication

---
+++ app/src/main/java/com/example/bookapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.bookapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button go_to_page = (Button)findViewById(R.id.go_to_page);
+    }
+
+    public void goToPage(View v) {
+        Intent intent = new Intent(MainActivity.this, PageActivity.class);
+        startActivity(intent);
+
+        intent.putExtra("page_number", "1");
+        //intent.putExtra("back", "new_pallette");
+    }
+}
---
+++ app/src/main/java/com/example/bookapplication/Page.java
@@ -0,0 +1,7 @@
+package com.example.bookapplication;
+
+public class Page {
+    int page_number;
+    boolean animation = false;
+
+}
